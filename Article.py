dd6890da531f369c80b0277c70b569ebd2b2e9ec
thomasbostaji <thomas.bostaji@student.vinci.be>
2024-03-22 12:30:45 +0100

    add super for getters

---
+++ Article.py
@@ -0,0 +1,38 @@
+from typing import Dict, List
+
+class Article:
+
+    articles: Dict[str, List[object]] = {
+        "bd": [],
+        "toys": []
+    }
+
+    def __init__(self, code_barre:str, prix:int, stock:int = 1 ) -> None:
+        self._prix :int = prix
+        self._stock:int = stock
+        self._code_barre:str = code_barre
+
+    @property
+    def prix(self) -> int:
+        return self._prix
+
+    @prix.setter
+    def prix(self, value: int) -> None:
+        self._prix = value
+
+    @property
+    def stock(self) -> int:
+        return self._stock
+
+    @stock.setter
+    def stock(self, value: int) -> None:
+        self._stock = value
+
+    @property
+    def code_barre(self) -> str:
+        return self._code_barre
+
+    @code_barre.setter
+    def code_barre(self, value: str) -> None:
+        self._code_barre = value    
+            
---
+++ BD.py
@@ -0,0 +1,60 @@
+import Article
+
+class BD(Article):
+
+    def __init__(self, title: str, auteur_name: str, isbn: str, first_publish_year: str, language: str, code_barre: str, prix:int) -> None:
+        super().__init__(code_barre, prix)
+        self._title: str = title
+        self._auteur_name: str = auteur_name
+        self._isbn: str = isbn
+        self._first_publish_year: str = first_publish_year
+        self._language: str = language
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        self._title = value
+
+    @property
+    def auteur_name(self):
+        return self._auteur_name
+
+    @auteur_name.setter
+    def auteur_name(self, value):
+        self._auteur_name = value
+
+    @property
+    def isbn(self):
+        return self._isbn
+
+    @isbn.setter
+    def isbn(self, value):
+        self._isbn = value
+
+    @property
+    def first_publish_year(self):
+        return self._first_publish_year
+
+    @first_publish_year.setter
+    def first_publish_year(self, value):
+        self._first_publish_year = value
+
+    @property
+    def language(self):
+        return self._language
+
+    @language.setter
+    def language(self, value):
+        self._language = value
+
+    def prix(self):
+        return super().prix  # Appel du getter de prix de la classe parente
+    
+    def stock(self):
+        return super().stock  # Appel du getter de stock de la classe parente
+    
+    def code_barre(self):
+        return super().code_barre  # Appel du getter de code_barre de la classe parente
